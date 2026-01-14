eb1428bcbfd5df7c3048e81abff7c1ac72bbbee1
joel.neto <joelneto441@gmail.com>
2026-01-13 23:19:44 -0300

    initial commit

---
+++ src/main/java/com/household/finance/CaixinhaAppApplication.java
@@ -0,0 +1,13 @@
+package com.household.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaixinhaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaixinhaAppApplication.class, args);
+	}
+
+}
